5df57d03a529fb60c1576639d787d546a778e5cd
rame0 <r@me0.biz>
2019-11-02 06:40:52 +0300

    Examples

---
+++ examples/apikey_example.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of UnisenderORM.
+ *
+ * 2019 (c) Ramil Aliyakberov (RAMe0) <r@me0.biz>
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+
+// This is base file for examples.
+// Make copy of it  with name 'apikey.php' and put you api key there
+$apikey = '<your_key_here>';
---
+++ examples/first.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of UnisenderORM.
+ *
+ * 2019 (c) Ramil Aliyakberov (RAMe0) <r@me0.biz>
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+
+use rame0\UniORM\Classes\ContactList;
+use rame0\UniORM\Classes\Exceptions\ORMException;
+use rame0\UniORM\ORM;
+
+require_once '../vendor/autoload.php';
+require_once 'apikey.php';
+
+try {
+    ORM::config($apikey);
+    $inst = ORM::getInstance();
+
+    $response = ContactList::get();
+    var_dump(ORM::getRequestErrorLog());
+    var_dump($response->getCollection());
+
+    $response->delete(719386, true);
+    var_dump($response->getCollection());
+} catch (ORMException $ex) {
+    var_dump(ORM::getRequestWarnLog());
+    var_dump(ORM::getRequestErrorLog());
+}
